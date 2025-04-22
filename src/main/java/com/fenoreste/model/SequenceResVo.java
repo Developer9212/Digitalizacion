77f1f0710f697913d774cb45b89148463e906aa0
Developer9212 <jdalton201214@gmail.com>
2025-04-22 14:58:55 -0600

    22/04/2025

--- src/main/java/com/fenoreste/model/SequenceResVo.java
+++ src/main/java/com/fenoreste/model/SequenceResVo.java
@@ -1,5 +1,6 @@
 package com.fenoreste.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,14 +10,13 @@ import java.io.Serializable;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class SequenceResVo implements Serializable {
 
     private Integer question;
     private Integer question_id;
     private String name;
     private String mandatory;
-    private String url;
-    private String value;
 
     private static final long serialVersionUID = 1L;
 }
