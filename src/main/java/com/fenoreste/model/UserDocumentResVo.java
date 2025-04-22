5cfd992a9d5236f7fc38ec2ecb26b6497c911e63
Developer9212 <jdalton201214@gmail.com>
2025-04-22 14:53:47 -0600

    22/04/2025

--- src/main/java/com/fenoreste/model/UserDocumentResVo.java
+++ src/main/java/com/fenoreste/model/UserDocumentResVo.java
@@ -16,17 +16,15 @@ public class UserDocumentResVo implements Serializable {
 
     private String id;
     private String organization_id;
-    private String organization_document_id;
     private String user_id;
     private String name;
     private String external_id;
     private List<SequenceResVo> sequence;
     private boolean completed;
-    private Integer signature_progress;
     private boolean workflow;
     private String updated_at;
     private String created_at;
-    private String config_temp;
+
 
     private static final long serialVersionUID = 1L;
 }
